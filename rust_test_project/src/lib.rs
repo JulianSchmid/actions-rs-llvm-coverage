bd70e82bfca19d0d6690d581e2cbb5bd5c1850e7
Julian Schmid <info@julianschmid.name>
2020-12-28 14:02:22 +0100

    Added some code that should result in missing coverage

--- rust_test_project/src/lib.rs
+++ rust_test_project/src/lib.rs
@@ -3,10 +3,29 @@ pub fn some_func() -> u64 {
     1
 }
 
+pub fn some_result(some: u64) -> Result<u64,bool> {
+    if some > 10 {
+        Err(some % 2 == 0)
+    } else {
+        Ok(some)
+    }
+}
+
+pub fn some_result_return_using_function(some: u64) -> Result<bool,bool> {
+    let re = some_result(some)?;
+    Ok(re > 5)
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn some_result() {
+        // check only the non error case (should result in some missing coverage)
+        assert_eq!(false, crate::some_result_return_using_function(4).unwrap());
     }
 }
